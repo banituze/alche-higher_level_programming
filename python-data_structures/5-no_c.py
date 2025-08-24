3829beeb45cc71096332465203e8bbd7a2f4a689
Winebald Banituze <w.banituze@alustudent.com>
2025-08-24 11:43:02 +0400

    updated 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    new_string = ""
-
-    for i in my_string:
-        if i == 'c' or i == "C":
-            pass
-        else:
-            new_string += i
-    return new_string
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
