897f00460bf0e85d090e098b51319fc953d91b27
Winebald <w.banituze@alustudent.com>
2025-09-11 07:49:19 -0700

    updated 101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -7,31 +7,30 @@ def print_pretty(size, code_dict):
     """parse important data"""
     print("File size: {}".format(size))
     for key, value in sorted(code_dict.items()):
-        if (value != 0):
+        if value != 0:
             print("{}: {}".format(key, value))
+
 
 if __name__ == '__main__':
     """init code to print the parsed data"""
     size = 0
-    code_dict = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+    code_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+                 "403": 0, "404": 0, "405": 0, "500": 0}
     try:
         line_counter = 0
         for line in sys.stdin:
+            parts = line.split()
+            if len(parts) < 9:
+                continue
             line_counter += 1
-            code = line.split()[7]
-            size += int(line.split()[8])
+            code = parts[7]
+            try:
+                size += int(parts[8])
+            except ValueError:
+                continue
             if code in code_dict:
                 code_dict[code] += 1
-            if (line_counter % 10 == 0):
+            if line_counter % 10 == 0:
                 print_pretty(size, code_dict)
         print_pretty(size, code_dict)
     except KeyboardInterrupt:
