b4501cf7de431e3076b9e5b5688d2ae0936d4e90
Winebald <w.banituze@alustudent.com>
2025-09-19 08:49:06 -0700

    updated 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,8 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alu-intranet.hbtn.io/status')
+    r = requests.get('https://intranet.hbtn.io/status')
+    if r.startswith('https://'):
+        r = "https://alu-intranet.hbtn.io/status"
     t = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
