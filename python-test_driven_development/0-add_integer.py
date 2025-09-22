dca4671a8d8554c909cbcda48fc3afba7f97c696
Winebald <w.banituze@alustudent.com>
2025-09-22 05:54:25 -0700

    updated 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,8 @@ def add_integer(a, b=98):
         a - first number input
         b - second number input
     """
-    if type(a) not in [int, float]:
+    if type(a) not in [int] and type(a) not in [float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in [int] and type(b) not in [float]:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
