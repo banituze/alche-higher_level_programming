65ccd9dc21048c6785d94862ce1e1cc3108c4cee
Winebald <w.banituze@alustudent.com>
2025-09-23 08:34:50 -0700

    updated 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     from sys import argv
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
