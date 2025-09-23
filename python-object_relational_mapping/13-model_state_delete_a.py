cff459749f3f5c2e92330064330879543bdc756b
Winebald <w.banituze@alustudent.com>
2025-09-23 08:57:19 -0700

    updated 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
